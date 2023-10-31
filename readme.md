aadb5675da2b6a416d57b64805c50f50683540b9
KilRol <sniper.lech@gmail.com>
2023-10-31 20:48:59 +0300

    add readme

---
+++ readme.md
@@ -0,0 +1,8 @@
+## Скрипт для расчета результата матчей
+
+1. Cклонировать репозиторий 
+2. Открыть VS Code
+3. Установить расширение [LiveServer](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
+4. Нажать Go Live справа снизу (на этом шаге должен открыться браузер по адресу: http://127.0.0.1:5500/)
+
+
--- src/components/MainTable/getMainTable.js
+++ src/components/MainTable/getMainTable.js
@@ -5,34 +5,41 @@ import { getGridTableButton } from "../GridTableButton/GridTableButton.js";
 import { GridTable } from "../GridTable/index.js";
 
 export async function getMainTable(url) {
-    const tableWrapper = document.createElement("div");
-    const results = await getJSON(url).then(res => res.filter((item) => item.score1 !== '-'));
-    const jsonedResult = getResults(results);
+  const tableWrapper = document.createElement("div");
+  const results = await getJSON(url).then((res) =>
+    res.filter((item) => item.score1 !== "-")
+  );
+  const jsonedResult = getResults(results);
+
+  const expand = () => {
+    tableWrapper.classList.add("collapse-item__active");
+  };
   
-    const table = getTable(results);
-    const show_btn = getGridTableButton();
-    const grid = GridTable(results, jsonedResult);
-    tableWrapper.appendChild(table);
-    tableWrapper.appendChild(show_btn);
-    tableWrapper.appendChild(grid);
-  
-    tableWrapper.style.width = "60%";
-    tableWrapper.style.display = "flex";
-    tableWrapper.style.flexDirection = "column";
-    tableWrapper.style.justifyContent = "space-between";
-    tableWrapper.style.alignItems = "center";
-  
-    const textMain = document.createElement("textarea");
-    textMain.innerText = tableWrapper.innerHTML;
-    textMain.style.width = "500px";
-    textMain.style.height = "400px";
-  
-    const wrapper = document.createElement("div");
-    wrapper.style.display = "flex";
-    wrapper.style.justifyContent = "space-around";
-  
-    wrapper.appendChild(tableWrapper);
-    wrapper.appendChild(textMain);
-  
-    return wrapper;
-  }
+  const table = getTable(results);
+  const show_btn = getGridTableButton(expand);
+  const grid = GridTable(results, jsonedResult);
+
+  tableWrapper.appendChild(table);
+  tableWrapper.appendChild(show_btn);
+  tableWrapper.appendChild(grid);
+
+  tableWrapper.style.width = "60%";
+  tableWrapper.style.display = "flex";
+  tableWrapper.style.flexDirection = "column";
+  tableWrapper.style.justifyContent = "space-between";
+  tableWrapper.style.alignItems = "center";
+
+  const textMain = document.createElement("textarea");
+  textMain.innerText = tableWrapper.innerHTML;
+  textMain.style.width = "500px";
+  textMain.style.height = "400px";
+
+  const wrapper = document.createElement("div");
+  wrapper.style.display = "flex";
+  wrapper.style.justifyContent = "space-around";
+
+  wrapper.appendChild(tableWrapper);
+  wrapper.appendChild(textMain);
+
+  return wrapper;
+}
